75414d646ebfb18d1c719cc034d07973173b72b1
Aaron Straup Cope <straup@gmail.com>
2009-09-14 00:00:31 -0700

    tfl wranging

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from google.appengine.ext import webapp
 
 import fakesubwayapis.bart
 import fakesubwayapis.stm
+import fakesubwayapis.tfl
+import fakesubwayapis.ukrail
 
 if __name__ == '__main__':
 
   handlers = [
     (r'/bart/getinfo/([a-z0-9]+)', fakesubwayapis.bart.getinfo),
-    (r'/stm/getinfo/(m\d{2})', fakesubwayapis.stm.getinfo),    
+    (r'/stm/getinfo/(m\d{2})', fakesubwayapis.stm.getinfo),
+    (r'/tfl/getinfo/(\w+)(?:-(\w))?', fakesubwayapis.tfl.getinfo),    
+    (r'/ukrail/getinfo/(\w{3})', fakesubwayapis.ukrail.getinfo),
     ]
 
   application = webapp.WSGIApplication(handlers, debug=False)
